270895b357b32b35325487d3172a92350b555178
irozaji <ilzat.rozaji@gmail.com>
2025-08-28 11:48:08 -0400

    cursor rules

--- SnapPost/ContentView.swift
+++ SnapPost/ContentView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+        
+    var a:Int = 1;
+    
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hi Ilzat")
+            Text("Hi Ilzat" + String(a))
         }
         .padding()
     }
